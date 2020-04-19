41ee3b2f255c293095ae61f3bcf3379e786ba01a
FredPerr <frederic.norrep@gmail.com>
2020-04-19 19:38:02 -0400

    Added project base for spigot plugin.

---
+++ src/main/java/ca/fredperr/pluginprime/Prime.java
@@ -0,0 +1,15 @@
+package ca.fredperr.pluginprime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Prime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    public void onDisable(){
+
+    }
+}
